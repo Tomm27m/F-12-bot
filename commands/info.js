bf30609905c23b583b7b83e842b5f9d2efef593e
kim <74242336+imstraightasaline@users.noreply.github.com>
2024-03-19 04:26:55 +0000

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -7,13 +7,13 @@ module.exports = {
   testOnly: true,
 
   callback: ({ message, interaction }) => {
-    var gavin = "530188818733727774";
-    var tom = "578935696518152193";
+    var marcy = "530188818733727774";
+    var me = "578935696518152193";
 
     const infoEmbed = new MessageEmbed()
     .setColor('RANDOM')
     .setTitle('F-12 - Info')
-    .setDescription(`F-12 is a free bot that keeps track of scams spread throughout Discord. Our bot scans every message sent if it includes a malicious link.\n\nF-12 was created by <@${tom}> and <@${gavin}>.\nIf you would like to support the growth of our bot, click [here](https://discord.gg/XyqZd7thdB)!`)
+    .setDescription(`F-12 is a free bot that keeps track of scams spread throughout Discord. Our bot scans every message sent if it includes a malicious link.\n\nF-12 was created by <@${me}> and <@${marcy}>.\nIf you would like to support the growth of our bot, click [here](https://discord.gg/XyqZd7thdB)!`)
     .setThumbnail('https://cdn.discordapp.com/attachments/907903436874452993/929269025349984316/F-12.png')
     .setTimestamp();
 
