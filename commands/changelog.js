d9ec5bba9f275c330e0f6683679a4d8fee6e1f05
kim <74242336+imstraightasaline@users.noreply.github.com>
2024-03-19 04:24:58 +0000

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,7 +1,7 @@
 module.exports = {
   execute (os, client, message, MessageEmbed){
 
-    message.channel.send('Bot discontinued. Read the docs below for more info.\nhttps://tomm27m.gitbook.io/f-12/f-12/bot-discontinued.');
+    message.channel.send('Bot discontinued. Read the docs below for more info.\nhttps://######.gitbook.io/f-12/f-12/bot-discontinued.');
     return;
   }
 }
