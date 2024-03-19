fb932c1d474d9e7d6e841a0add790b86c7260a7a
kim <74242336+imstraightasaline@users.noreply.github.com>
2024-03-19 04:25:33 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 module.exports = {
     execute (os, client, message){
-        message.reply('Bot discontinued. Read the docs below for more info.\nhttps://tomm27m.gitbook.io/f-12/f-12/bot-discontinued.');
+        message.reply('Bot discontinued. Read the docs below for more info.\nhttps://#######.gitbook.io/f-12/f-12/bot-discontinued.');
         return;
     }
 }
