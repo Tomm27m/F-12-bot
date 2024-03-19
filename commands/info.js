828aff50e12eeac2464004eeda7a706c56802989
kim <74242336+imstraightasaline@users.noreply.github.com>
2024-03-19 04:25:22 +0000

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,6 +1,6 @@
 module.exports = {
     execute (os, client, message, MessageEmbed){
-        message.channel.send('Bot discontinued. Read the docs below for more info.\nhttps://tomm27m.gitbook.io/f-12/f-12/bot-discontinued.');
+        message.channel.send('Bot discontinued. Read the docs below for more info.\nhttps://#######.gitbook.io/f-12/f-12/bot-discontinued.');
         return;
     }
 }
